b8e8215dd4156fd0a9542737ec918c173ab97617
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-03-09 10:53:09 -0800

    Co-authored-by: @jidemobell & @BirdyArt

---
+++ src/components/Login.svelte
@@ -0,0 +1,69 @@
+<script>
+  import { focus } from "@roxi/routify";
+  let email = "";
+  let password = "";
+
+  const labelHandler = (e) => e.target.previousElementSibling.focus();
+</script>
+
+<div class="shadow-xl p-10 bg-white max-w-xl rounded">
+  <h1 class="text-4xl font-black mb-4">Login</h1>
+  <div class="mb-4 relative">
+    <input
+      use:focus
+      class="input border border-gray-400 appearance-none rounded w-full px-3 py-3 pt-5 pb-2 focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600"
+      bind:value={email}
+      class:filled={email}
+      id="email"
+      type="text"
+    />
+    <label
+      on:click={labelHandler}
+      for="email"
+      class="label absolute mb-0 -mt-2 pt-4 pl-3 leading-tighter text-gray-400 text-base mt-2 cursor-text"
+      >Email Address
+    </label>
+  </div>
+  <div class="mb-4 relative">
+    <input
+      class="input border border-gray-400 appearance-none rounded w-full px-3 py-3 pt-5 pb-2 focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600"
+      bind:value={password}
+      class:filled={password}
+      id="password"
+      type="password"
+    />
+    <label
+      on:click={labelHandler}
+      for="password"
+      class="label absolute mb-0 -mt-2 pt-4 pl-3 leading-tighter text-gray-400 text-base mt-2 cursor-text"
+      >Password</label
+    >
+  </div>
+  <button
+    class="bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded"
+    >Submit</button
+  >
+</div>
+
+<!-- component -->
+<style>
+  .input {
+    transition: border 0.2s ease-in-out;
+    min-width: 280px;
+  }
+
+  .input:focus + .label,
+  .input:active + .label,
+  .input.filled + .label {
+    font-size: 0.75rem;
+    transition: all 0.2s ease-out;
+    top: -1.2rem;
+    color: #667eea;
+  }
+
+  .label {
+    transition: all 0.2s ease-out;
+    top: 0.4rem;
+    left: 0;
+  }
+</style>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -3,8 +3,11 @@
   // import { metatags } from '@roxi/routify'
   // metatags.title = 'My Routify app'
   // metatags.description = 'Description coming soon...'
+  import Login from "../components/Login.svelte";
 </script>
 
-<h1 class="bg-red-100 text-blue-200">
+<!-- <h1 class="bg-red-100 text-blue-500">
   Add your tailwind classes to see HMR in action!
-</h1>
+</h1> -->
+
+<Login />

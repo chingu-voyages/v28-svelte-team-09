42d50e26bdc0665327f46bd05f041275bcec0a89
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-03-12 00:17:54 -0800

    import initClient as TODO

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,9 @@
 <script>
   import { Router } from "@roxi/routify";
   import { routes } from "../.routify/routes";
+  // import { initClient } from "./utils/client";
+  // TODO: init client w/ secret
+  // secret && initClient(secret)
 </script>
 
 <Router {routes} />

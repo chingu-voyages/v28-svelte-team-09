7d4b18dac4f0ce1846410d803ae7b5be2b23cdf8
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-03-13 13:29:33 -0800

    changes to button API

--- src/components/dev/Button.svelte
+++ src/components/dev/Button.svelte
@@ -1,3 +1,25 @@
-<button class="bg-indigo-500 shadow-md cursor-pointer  hover:bg-indigo-400 focus:outline-none text-white font-semibold py-2 px-4 border border-indigo-500 hover:border-indigo-400 rounded">
-  <slot />
-</button>
+<script>
+  export let shadow;
+  export let type = "filled";
+</script>
+
+<!-- TODO: Add styles for input submit type if needed based on conditions -->
+<button
+  class="rounded font-semibold py-2 px-4 border border-indigo-500 cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+  class:filled={type == "filled"}
+  class:outline={type == "outline"}
+  class:shadow
+  on:click><slot /></button
+>
+
+<style>
+  .outline {
+    @apply bg-transparent text-indigo-500 hover:bg-indigo-300 hover:border-indigo-300 hover:text-white;
+  }
+  .filled {
+    @apply hover:bg-indigo-400 hover:border-indigo-400 bg-indigo-500 text-white;
+  }
+  .shadow {
+    @apply shadow-md;
+  }
+</style>

6d2f18e65a280727fc651c79ed0c01235e362133
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-03-13 13:57:32 -0800

    document components

--- src/components/dev/Button.svelte
+++ src/components/dev/Button.svelte
@@ -3,6 +3,11 @@
   export let type = "filled";
 </script>
 
+<!--@component
+@type ="outline" for variant
+@shadow box-shadow
+-->
+
 <!-- TODO: Add styles for input submit type if needed based on conditions -->
 <button
   class="rounded font-semibold py-2 px-4 border border-indigo-500 cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"

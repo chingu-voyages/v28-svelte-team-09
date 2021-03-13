82f0aaef97fceb37677436f915c14f297f3c38dc
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-03-13 13:46:27 -0800

    rm unused component

--- src/components/dev/ButtonOutline.svelte
+++
@@ -1,3 +0,0 @@
-<button class="bg-transparent shadow-md cursor-pointer hover:bg-indigo-500 focus:outline-none text-indigo-500 focus:border-indigo-500 font-semibold hover:text-white py-2 px-4 border border-indigo-500 hover:border-transparent rounded">
-  <slot />
-</button>

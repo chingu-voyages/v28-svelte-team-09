ceaff7d24133a162e571575204ab760963e6dea7
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-03-12 19:33:22 -0800

    Fix login component labels

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -11,33 +11,36 @@
   <div class="mb-4 relative">
     <input
       use:focus
-      class="input border border-gray-400 appearance-none rounded w-full px-3 py-3 pt-5 pb-2 focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600"
+      class="[ input ] border border-gray-400 appearance-none rounded w-full px-3 py-3 pt-5 pb-2 focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600"
       bind:value={email}
       class:filled={email}
       id="email"
       type="text"
+      aria-labelledby="email-label"
     />
-    <label
+    <span
       on:click={labelHandler}
-      for="email"
-      class="label absolute mb-0 -mt-2 pt-4 pl-3 leading-tighter text-gray-400 text-base mt-2 cursor-text"
+      id="email-label"
+      class="[ label ] absolute mb-0 -mt-2 pt-4 pl-3 leading-tighter text-gray-400 text-base mt-2 cursor-text"
       >Email Address
-    </label>
+    </span>
   </div>
   <div class="mb-4 relative">
     <input
-      class="input border border-gray-400 appearance-none rounded w-full px-3 py-3 pt-5 pb-2 focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600"
+      class="[ input ] border border-gray-400 appearance-none rounded w-full px-3 py-3 pt-5 pb-2 focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600"
       bind:value={password}
       class:filled={password}
       id="password"
       type="password"
+      aria-labelledby="password-label"
     />
-    <label
+    <span
       on:click={labelHandler}
-      for="password"
-      class="label absolute mb-0 -mt-2 pt-4 pl-3 leading-tighter text-gray-400 text-base mt-2 cursor-text"
-      >Password</label
+      id="password-label"
+      class="[ label ] absolute mb-0 -mt-2 pt-4 pl-3 leading-tighter text-gray-400 text-base mt-2 cursor-text"
     >
+      Password
+    </span>
   </div>
   <button
     class="bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded"
@@ -45,7 +48,6 @@
   >
 </div>
 
-<!-- component -->
 <style>
   .input {
     transition: border 0.2s ease-in-out;
@@ -57,13 +59,13 @@
   .input.filled + .label {
     font-size: 0.75rem;
     transition: all 0.2s ease-out;
-    top: -1.2rem;
+    top: -1.6rem;
     color: #667eea;
   }
 
   .label {
     transition: all 0.2s ease-out;
-    top: 0.4rem;
+    top: -0.8rem;
     left: 0;
   }
 </style>

e2920b328686076b988c8a27da0b2e567ed19d72
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-03-14 09:43:48 -0700

    more specific selector

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -2,11 +2,8 @@
 <slot />
 
 <style>
-  :global(*) {
+  :global(*:not(input[type="text"])) {
     @apply font-main text-indigo-500;
     letter-spacing: 1px;
-  }
-  :global(input[type="text"]) {
-    @apply text-black;
   }
 </style>

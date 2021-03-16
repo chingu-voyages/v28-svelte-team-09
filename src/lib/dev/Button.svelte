4efd64599b81e8caa2349b7425554f0b79e42f9e
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-03-16 08:25:23 -0700

    simplify API

--- src/lib/dev/Button.svelte
+++ src/lib/dev/Button.svelte
@@ -1,5 +1,5 @@
 <script>
-  export let shadow;
+  export let shadow = false;
   export let type = "filled";
 </script>
 
@@ -13,7 +13,7 @@
   class="rounded font-semibold py-2 px-4 border border-indigo-500 cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
   class:filled={type == "filled"}
   class:outline={type == "outline"}
-  class:shadow
+  class:shadow-md={shadow}
   on:click><slot /></button
 >
 
@@ -23,8 +23,5 @@
   }
   .filled {
     @apply hover:bg-indigo-400 hover:border-indigo-400 bg-indigo-500 text-white;
-  }
-  .shadow {
-    @apply shadow-md;
   }
 </style>

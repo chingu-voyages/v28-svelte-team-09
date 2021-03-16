371cbe6cb9cd51b4ac26f96d239ac52dff9a2b8f
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-03-16 08:58:35 -0700

    resize logo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <header class="flex row items-center justify-between px-4 lg:px-20 pt-4">
-  <h2 class="justify-self-start font-logo text-xl font-semibold">DShift</h2>
+  <h2 class="justify-self-start font-logo text-2xl font-semibold">DShift</h2>
   <nav>
     <a href="/signin">
       <Button>Login</Button>

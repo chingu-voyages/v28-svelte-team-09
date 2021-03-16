43e1c1958a72b1bd38f1d0544a3461062f183856
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-03-16 01:01:09 -0700

    set layout back while we debug

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,8 +1,8 @@
 <script>
   import "../global.css";
-  import { initClient } from "../utils/client";
+  // import { initClient } from "../utils/client";
   // TODO: init client w/ secret
-  initClient("da hello");
+  // secret && initClient(secret)
 </script>
 
 <slot />

a28a333f566ff8af3e2223ab006a9f4dcf76491c
BirdyArt <asobolev91@gmail.com>
2021-03-17 17:53:03 -0700

    moved signin and signup to index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,23 @@
 <script>
   import Button from "$lib/Button.svelte";
+  import ModalBox from "$lib/ModalBox.svelte";
+  import AuthForm from "$lib/AuthForm.svelte";
   import { authStore } from "$stores/auth";
-
-  let open = true;
 </script>
 
 <header class="flex row items-center justify-between px-4 lg:px-20 pt-4">
   <h2 class="justify-self-start font-logo text-2xl font-semibold">DShift</h2>
   <nav>
-    <a href="/signin">
       <Button>Login</Button>
-    </a>
-    <a href="/signup">
       <Button type="outline">Register</Button>
-    </a>
   </nav>
 </header>
+
+<main>
+  <ModalBox clickOutside={true} open={true}>
+    <AuthForm login={true} />
+  </ModalBox>
+</main>
 
 {#if $authStore?.secret}
   <div style="display:none;">

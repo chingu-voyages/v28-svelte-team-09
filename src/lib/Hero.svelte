29b0e26c2cdd816c4b5b2ad51e294a697e75cfb9
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-03-21 14:18:10 -0700

    fix mobile cutoff

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,4 +1,5 @@
-<script>let open = false;
+<script>
+  let open = false;
   import ModalBox from "$lib/ModalBox.svelte";
   import AuthForm from "$lib/AuthForm.svelte";
 
@@ -9,16 +10,20 @@
     !(clickOutside = false) && setTimeout(() => (clickOutside = true));
 </script>
 
-<header class="pt-20 bg-indigo-500 relative"
->
-  <div class="container text-center mx-auto py-6">
-    <h1 class="text-5xl font-semibold text-white">Shift planning done simple</h1>
-    <hr class="my-5 mx-28 border-yellow	border-2 rounded-full">
+<header class="pt-20 bg-indigo-500 relative">
+  <div class="container text-center mx-auto py-6 w-11/12">
+    <h1 class="text-5xl font-semibold text-white">
+      Shift planning done simple
+    </h1>
+    <hr class="my-5 mx-28 border-yellow	border-2 rounded-full" />
     <h3 class="text-2xl mb-8 text-white">
-      Scheduling and timesheets without the usual chaos. Just Drag, Drop and Done.
+      Scheduling and timesheets without the usual chaos. Just Drag, Drop and
+      Done.
     </h3>
-    <button on:click={() => (open = true) && (login = false)} class="rounded-full shadow-xl text-xl font-semibold bg-white py-3 px-6  cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow
-      ">
+    <button
+      on:click={() => (open = true) && (login = false)}
+      class="rounded-full shadow-xl text-xl font-semibold bg-white py-3 px-6 cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow"
+    >
       Join Today
     </button>
     <ModalBox {clickOutside} bind:open>
@@ -28,42 +33,56 @@
 </header>
 
 <main class="text-center bg-indigo-500 relative md:pb-20">
-  <div class="main_bg"></div>
+  <div class="main_bg" />
   <div class="container mx-auto relative">
-    <div class="hidden md:grid grid-cols-1 justify-items-center md:justify-items-stretch md:grid-cols-4 p-5 lg:px-10"> 
-      <div class="col-span-3 rounded overflow-hidden border-indigo-500 border-2 shadow-lg mx-5 my-5">
-        <img class="mx-auto" src="/images/schedule.webp" alt="Desktop">
+    <div
+      class="hidden md:grid grid-cols-1 justify-items-center md:justify-items-stretch md:grid-cols-4 p-5 lg:px-10"
+    >
+      <div
+        class="col-span-3 rounded overflow-hidden border-indigo-500 border-2 shadow-lg mx-5 my-5"
+      >
+        <img class="mx-auto" src="/images/schedule.webp" alt="Desktop" />
       </div>
       <div class="col-span-1 mx-5 my-5">
-        <img class="mx-auto shadow-lg rounded-3xl border-indigo-500 border-2" src="/images/phone.webp" alt="Schedule">
+        <img
+          class="mx-auto shadow-lg rounded-3xl border-indigo-500 border-2"
+          src="/images/phone.webp"
+          alt="Schedule"
+        />
       </div>
     </div>
-    <div class="grid grid-cols-1 justify-items-center md:grid-cols-3 p-5 lg:px-10">  
-      <div class="rounded w-10/12 md:w-11/12 bg-white border-indigo-500 border-2 shadow-lg mx-5 my-5">
-        <img class="mx-auto pt-4 icon" src="/images/minutes.svg" alt="Schedule">
+    <div
+      class="grid grid-cols-1 justify-items-center md:grid-cols-3 p-5 lg:px-10"
+    >
+      <div
+        class="rounded w-10/12 md:w-11/12 bg-white border-indigo-500 border-2 shadow-lg mx-5 my-5"
+      >
+        <img
+          class="mx-auto pt-4 icon"
+          src="/images/minutes.svg"
+          alt="Schedule"
+        />
         <div class="px-6 py-4">
           <div class="font-bold text-xl mb-2">Schedule in minutes</div>
-          <p>
-            Create the best schedule for your team, fast
-          </p>
+          <p>Create the best schedule for your team, fast</p>
         </div>
       </div>
-      <div class="rounded w-10/12 bg-white border-indigo-500 border-2 shadow-lg mx-5 my-5">
-        <img class="mx-auto pt-4 icon" src="/images/mail.svg" alt="Mail">
+      <div
+        class="rounded w-10/12 bg-white border-indigo-500 border-2 shadow-lg mx-5 my-5"
+      >
+        <img class="mx-auto pt-4 icon" src="/images/mail.svg" alt="Mail" />
         <div class="px-6 py-4">
           <div class="font-bold text-xl mb-2">Send it to team</div>
-          <p>
-            Share the schedule you created in one click
-          </p>
+          <p>Share the schedule you created in one click</p>
         </div>
       </div>
-      <div class="rounded w-10/12 bg-white border-indigo-500 border-2 shadow-lg mx-5 my-5">
-        <img class="mx-auto pt-4 icon" src="/images/person.svg" alt="Person">
+      <div
+        class="rounded w-10/12 bg-white border-indigo-500 border-2 shadow-lg mx-5 my-5"
+      >
+        <img class="mx-auto pt-4 icon" src="/images/person.svg" alt="Person" />
         <div class="px-6 py-4">
           <div class="font-bold text-xl mb-2">Create staff profiles</div>
-          <p>
-            Add icon, position, and other team member info
-          </p>
+          <p>Add icon, position, and other team member info</p>
         </div>
       </div>
     </div>
@@ -73,7 +92,7 @@
 <footer class="text-center bg-indigo-500">
   <div class="container mx-auto">
     <h2 class="font-logo text-white text-2xl font-medium">DShift</h2>
-    <hr class="my-5 mx-28 border-yellow	border-2 rounded-full">
+    <hr class="my-5 mx-28 border-yellow	border-2 rounded-full" />
     <h3 class="text-md pb-4 text-white">
       © DShift, Inc. 2021. Built with ❤️ by Svelte-Team-09
     </h3>

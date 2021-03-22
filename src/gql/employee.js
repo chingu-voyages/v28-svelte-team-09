53c793c39921db8f0de0c084b6d8d6a62bbc576b
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-03-21 17:36:29 -0700

    cleanup employee modal code

--- src/gql/employee.js
+++ src/gql/employee.js
@@ -1,55 +1,53 @@
 import { queryOp, mutationOp } from "$utils/client";
 import { gql } from "@urql/svelte";
 
-
-
-export const employeesByUserID = ({ id }) => 
+export const employeesByUserID = ({ id }) =>
   queryOp(
     gql`
-     query EmployessByUserID($id: ID!) {
-       result: findUserByID(id: $id){
-         employees{
-           data{
-             _id
-             name
-             manager {
-               name
-             }
-             email
-             phone
-           }
-         }
-       }
-     }
+      query EmployessByUserID($id: ID!) {
+        result: findUserByID(id: $id) {
+          employees {
+            data {
+              _id
+              name
+              manager {
+                name
+              }
+              email
+              phone
+            }
+          }
+        }
+      }
     `,
     { id }
-  )
+  );
 
-export const  useCreateEmployee = () => 
+export const useCreateEmployee = () =>
   mutationOp(
     gql`
-     mutation CreateEmployee(
-       $name: String!
-       $phone: String
-       $email: String!
-       $id: ID!
-     ){
-       result: createEmployee(
-         data: {
-           name: $name
-           phone: $phone
-           email: $email
-           manager: { connect: $id }
-         }
-       ){
-         _id
-         name
-         phone
-         email
-         manager {
-           name
-         }
-       }
-     }
+      mutation CreateEmployee(
+        $name: String!
+        $phone: String
+        $email: String!
+        $id: ID!
+      ) {
+        result: createEmployee(
+          data: {
+            name: $name
+            phone: $phone
+            email: $email
+            manager: { connect: $id }
+          }
+        ) {
+          _id
+          name
+          phone
+          email
+          manager {
+            name
+          }
+        }
+      }
     `
-  )
+  );

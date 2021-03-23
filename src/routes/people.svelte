ab4d4bd9a4a063f3fa649287b221e76a147ba182
BirdyArt <asobolev91@gmail.com>
2021-03-23 16:18:14 -0700

    Added people page and created page switch

---
+++ src/routes/people.svelte
@@ -0,0 +1,21 @@
+<script>
+  import AppHeader from "$lib/AppHeader.svelte";
+</script>
+
+<AppHeader />
+
+<main class="text-center bg-white">
+  <div><h1 class="my-44">Spacer</h1></div>
+</main>
+
+<footer class="text-center bg-indigo-500">
+  <div class="container mx-auto">
+    <h2 class="font-logo text-white text-2xl font-medium">DShift</h2>
+    <hr class="my-5 mx-28 border-yellow	border-2 rounded-full" />
+    <h3 class="text-md pb-4 text-white">
+      © DShift, Inc. 2021. Built with ❤️ by Svelte-Team-09
+    </h3>
+  </div>
+</footer>
+
+
--- src/routes/schedule.svelte
+++ src/routes/schedule.svelte
@@ -20,8 +20,19 @@
 
 <AppHeader />
 
-<main>
+<main class="text-center bg-white">
   <!--TODO:remove button. test button to try the add employee query -->
   <Button on:click={() => (addEmployee = true)}>Add Employee</Button>
   <AddEmployeeModal open={addEmployee} />
+  <div><h1 class="my-44">Spacer</h1></div>
 </main>
+
+<footer class="text-center bg-indigo-500">
+  <div class="container mx-auto">
+    <h2 class="font-logo text-white text-2xl font-medium">DShift</h2>
+    <hr class="my-5 mx-28 border-yellow	border-2 rounded-full" />
+    <h3 class="text-md pb-4 text-white">
+      © DShift, Inc. 2021. Built with ❤️ by Svelte-Team-09
+    </h3>
+  </div>
+</footer>

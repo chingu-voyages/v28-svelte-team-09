d986670adddf96d0ae8ba0143ed5bfc96ee672a6
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-03-24 14:36:12 -0700

    refactor button API

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,18 +1,20 @@
 <script>
-  export let shadow = false;
-  export let type = "filled";
+  export let shadow = false,
+    variant = "filled",
+    type = false;
 </script>
 
 <!--@component
-@type ="outline" for variant
+@variant `="outline"` for variant
 @shadow box-shadow
 -->
 
 <!-- TODO: Add styles for input submit type if needed based on conditions -->
 <button
   class="rounded font-semibold py-2 px-4 border border-indigo-500"
-  class:filled={type == "filled"}
-  class:outline={type == "outline"}
+  {type}
+  class:filled={variant == "filled"}
+  class:outline={variant == "outline"}
   class:shadow-md={shadow}
   on:click><slot /></button
 >

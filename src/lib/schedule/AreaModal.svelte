1e46712f960dd27ea6b2424dc8e3a5cfe59ca59b
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-03-28 16:08:31 -0700

    initial area modal

---
+++ src/lib/schedule/AreaModal.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Select from "./Select.svelte";
+</script>
+
+<Select label="Color" class="w-[4.7rem]" selectedIndex={0}
+  ><span slot="selected" class="block">
+    <div
+      class="ml-1 w-5 h-5 rounded-full border border-white"
+      style="background: green"
+    />
+  </span>
+  <div
+    class="w-4 h-4 rounded-full border border-white"
+    style="background: blue"
+    slot="item"
+    let:i
+    let:option={{ name, color }}
+  />
+</Select>

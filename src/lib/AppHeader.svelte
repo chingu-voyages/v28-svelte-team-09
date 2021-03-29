47f231d8a4ec8975255e7ae947e411122ad65481
BirdyArt <asobolev91@gmail.com>
2021-03-28 20:22:08 -0700

    adding profile initials

--- src/lib/AppHeader.svelte
+++ src/lib/AppHeader.svelte
@@ -15,7 +15,11 @@
     </div>
   </div>
 
-  <nav>
+  <nav class="flex items-center">
+    <div class="flex items-center cursor-pointer">
+      <!-- <div class="p-3 bg-indigo-300 icon rounded-full w-12 h-12 text-white font-semibold">{manager.name.match(/[A-Z]/g).join('')}</div>
+      <h1 class=" px-3 text-xl">{manager.name}</h1> -->
+    </div>
     <Button>
       <a
         href="/"
--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,13 +1,5 @@
 import { writable } from "svelte/store";
 
-/** Redirect if at a path that requires login and user is not logged in. */
-function checkPaths(arr) {
-  if (typeof window == "undefined") return;
-  var curPath = location.pathname;
-  if (!arr.filter((v) => curPath.includes(v) || curPath == "/").length)
-    location.pathname = `/signin`;
-}
-
 /** Initializes with auth details. */
 export const authStore = writable(
   (typeof window != "undefined" && JSON.parse(localStorage.getItem("auth")))

01522c711c810bd82a78a58d26de7a061f641761
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-03-28 21:06:57 -0700

    rm !important usage

--- src/lib/schedule/ShiftModal.svelte
+++ src/lib/schedule/ShiftModal.svelte
@@ -43,7 +43,7 @@
   >
     <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
       <div class="sm:flex sm:items-start">
-        <div class="mt-3 space-y-4 text-center sm:mt-0 sm:ml-4 sm:text-left">
+        <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
           <h3
             class="text-lg font-main leading-6 font-medium"
             id="modal-headline"
@@ -159,7 +159,7 @@
               />
             </div>
           </div>
-          <div class="!mt-8">
+          <div class="mt-4">
             <label
               for="notes"
               class="block text-sm font-medium text-indigo-400"

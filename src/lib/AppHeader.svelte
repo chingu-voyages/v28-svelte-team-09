b98affeb284204835aa68fe328b9516dc88a7d7b
BirdyArt <asobolev91@gmail.com>
2021-03-30 16:40:51 -0700

    removed page store

--- src/lib/AppHeader.svelte
+++ src/lib/AppHeader.svelte
@@ -1,7 +1,6 @@
 <script>
   import Button from "$lib/Button.svelte";
   import { authStore } from "$stores/auth";
-  import { page } from "$stores/pages";
 </script>
 
 <header class="flex row items-center justify-between px-4 lg:px-20">
@@ -10,8 +9,8 @@
     <img src="/images/logo.webp" alt="DShift logo" class="h-16 hidden md:block" />
     <h2 class="font-logo text-2xl font-medium hidden md:block">DShift</h2>
     <div class="mx-2 md:mx-5">
-      <a class="text-lg font-semibold p-5" class:selected="{$page === '/schedule' ? 'selected' : ''}" on:click={() => page.update(() => '/schedule')} href="/schedule">Schedule</a>
-      <a class="text-lg font-semibold p-5" class:selected="{$page === '/people' ? 'selected' : ''}" on:click={() => page.update(() => '/people')} href="/people">People</a>
+      <a class="text-lg font-semibold p-5" class:selected="{typeof window !== "undefined" && location.pathname === '/schedule' ? 'selected' : ''}" href="/schedule">Schedule</a>
+      <a class="text-lg font-semibold p-5" class:selected="{typeof window !== "undefined" && location.pathname === '/people' ? 'selected' : ''}" href="/people">People</a>
     </div>
   </div>
 

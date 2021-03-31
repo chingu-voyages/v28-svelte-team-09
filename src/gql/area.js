b27e113552f25028987cd9c0d88f98a94b7a2f96
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-03-31 14:31:09 -0700

    add all CRUD ops for area.js

--- src/gql/area.js
+++ src/gql/area.js
@@ -18,3 +18,36 @@ export const areasByUserID = ({ id }) =>
     `,
     { id }
   );
+
+export const useCreateArea = () =>
+  mutationOp(gql`
+    mutation CreateArea($id: ID!, $name: String!, $color: String!) {
+      result: createArea(
+        data: { name: $name, color: $color, company: { connect: $id } }
+      ) {
+        name
+        _id
+      }
+    }
+  `);
+
+export const useUpdateArea = () =>
+  mutationOp(gql`
+    mutation UpdateArea($id: ID!, $name: String, $color: String) {
+      partialUpdateArea(id: $id, data: { name: $name, color: $color }) {
+        name
+        color
+        _id
+      }
+    }
+  `);
+
+export const useDeleteArea = () =>
+  mutationOp(gql`
+    mutation DeleteArea($id: ID!) {
+      deleteArea(id: $id) {
+        _id
+        name
+      }
+    }
+  `);

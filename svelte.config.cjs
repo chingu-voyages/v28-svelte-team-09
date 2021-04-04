31ddaf07ba8cc0f3a15317f18ebfbae1a53747ea
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-04-04 13:54:07 -0700

    try a static deploy

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,4 +1,4 @@
-const netlify = require("@sveltejs/adapter-netlify");
+const static = require("@sveltejs/adapter-static");
 const sveltePreprocess = require("svelte-preprocess");
 const { resolve } = require("path");
 const pkg = require("./package.json");
@@ -18,7 +18,7 @@ module.exports = {
     // By default, `npm run build` will create a standard Netlify app.
     // You can create optimized builds for different platforms by
     // specifying a different adapter
-    adapter: netlify(),
+    adapter: static(),
 
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",

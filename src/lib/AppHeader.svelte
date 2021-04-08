1e5bdf7bafed6a7b4b4ce521dfd347f64ebc60a5
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-04-07 20:04:22 -0700

    fix focus styles

--- src/lib/AppHeader.svelte
+++ src/lib/AppHeader.svelte
@@ -1,35 +1,54 @@
 <script>
+  import { goto } from "$app/navigation";
   import Button from "$lib/Button.svelte";
   import { authStore } from "$stores/auth";
+  const aStyles =
+    "text-lg font-semibold p-5 focus:outline-none ring-inset focus:ring-4 focus:rounded focus:ring-indigo-500 transition-all";
 </script>
 
 <header class="flex row items-center justify-between px-4 lg:px-20">
   <!-- Logo -->
   <div class="flex items-center">
-    <img src="/images/logo.webp" alt="DShift logo" class="h-16 hidden md:block" />
+    <img
+      src="/images/logo.webp"
+      alt="DShift logo"
+      class="h-16 hidden md:block"
+    />
     <h2 class="font-logo text-2xl font-medium hidden md:block">DShift</h2>
     <div class="mx-2 md:mx-5">
-      <a class="text-lg font-semibold p-5" class:selected="{typeof window !== "undefined" && location.pathname === '/schedule' ? 'selected' : ''}" href="/schedule">Schedule</a>
-      <a class="text-lg font-semibold p-5" class:selected="{typeof window !== "undefined" && location.pathname === '/people' ? 'selected' : ''}" href="/people">People</a>
+      <a
+        class={aStyles}
+        class:selected={typeof window !== "undefined" &&
+        location.pathname === "/schedule"
+          ? "selected"
+          : ""}
+        href="/schedule">Schedule</a
+      >
+      <a
+        class={aStyles}
+        class:selected={typeof window !== "undefined" &&
+        location.pathname === "/people"
+          ? "selected"
+          : ""}
+        href="/people">People</a
+      >
     </div>
   </div>
 
   <nav class="flex items-center">
-      <a
-        href="/"
-        on:click={() => {
-          localStorage.removeItem("auth");
-          $authStore = null;
-        }}
-      ><Button>Logout</Button>
-        </a
-      >
+    <Button
+      on:click={() => {
+        localStorage.removeItem("auth");
+        $authStore = null;
+        goto("/");
+      }}>Logout</Button
+    >
   </nav>
 </header>
 
 <style>
   .selected {
-		background-color: #e0e7ff;
+    background-color: #e0e7ff;
     padding: 1.25rem;
-	}
+  }
 </style>

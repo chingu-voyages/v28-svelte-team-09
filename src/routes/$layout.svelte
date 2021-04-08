f08e07b53cf6c566bd4b08afd1a066120a7f3373
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-04-07 20:08:22 -0700

    add link to our repo

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -11,7 +11,13 @@
     <div class="container mx-auto">
       <h2 class="font-logo text-white text-2xl font-medium">DShift</h2>
       <hr class="my-5 mx-28 border-yellow	border-2 rounded-full" />
-      <h3 class="text-md pb-4 text-white">Built with ❤️ by Svelte-Team-09</h3>
+      <h3 class="text-md pb-4 text-white">
+        Built with ❤️ by <a
+          class="hover:underline"
+          href="https://github.com/chingu-voyages/v28-svelte-team-09"
+          >Svelte-Team-09</a
+        >
+      </h3>
     </div>
   </footer>
 </div>
